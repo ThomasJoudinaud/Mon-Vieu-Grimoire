595ba4d0b1b9763ffd2066e13e297eee4c7dc8a5
Thomas_J <106536735+ThomasJoudinaud@users.noreply.github.com>
2023-06-09 14:27:07 +0200

    ajout .env

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const bookRoutes = require("./routes/book")
 const userRoutes = require("./routes/user")
 const path = require("path")
 const rateLimit = require("express-rate-limit")
+require("dotenv").config()
 const app = express()
 
-mongoose.connect('mongodb+srv://thomas:opc@cluster0.j0vxr04.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true, useUnifiedTopology: true })
+const databaseURL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.j0vxr04.mongodb.net/?retryWrites=true&w=majority`;
+mongoose
+  .connect(databaseURL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
